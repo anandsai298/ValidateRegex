eec9bc4ff5f9de18ce6e7dbbd0439c63d864c621
anandsaikumar <anandsai298@gmail.com>
2023-03-27 09:18:18 +0530

    [sai] Add : Validation of pincode

---
+++ ValidateRegex/ValidateRegex/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace ValidateRegex;
+class program
+{
+    static void Main(string[] arg)
+    {
+        ValidatePinCode validatePinCode = new ValidatePinCode();
+        validatePinCode.Validate("533002");
+    }
+}
+
---
+++ ValidateRegex/ValidateRegex/ValidatePinCode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ValidateRegex
+{
+    public class ValidatePinCode
+    {
+        public const string PinCode = "^[0-9]{6}$";//string starts with ^ ends with $
+        public void Validate(string pinCode)
+        {
+            if(Regex.IsMatch(pinCode, PinCode))
+            {
+                Console.WriteLine("Validation is success");
+            }
+            else
+            {
+                Console.WriteLine("Validation is  not success");
+            }
+        }
+
+    }
+}
