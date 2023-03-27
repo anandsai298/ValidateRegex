283b3115423aed68fd382e0f0b5f0cbf5f835ac3
anandsaikumar <anandsai298@gmail.com>
2023-03-27 09:40:51 +0530

    [sai] Add : in this validate the pincode is invalid when the alphabets are given

--- ValidateRegex/ValidateRegex/Program.cs
+++ ValidateRegex/ValidateRegex/Program.cs
@@ -6,7 +6,10 @@ class program
     static void Main(string[] arg)
     {
         ValidatePinCode validatePinCode = new ValidatePinCode();
-        validatePinCode.Validate("533002");
+        validatePinCode.Validate("400088");
+        validatePinCode.Validate("A533002");
+        validatePinCode.Validate("533002A");
+        validatePinCode.Validate("400 088");
     }
 }
 
