74573f1dd94d19249aae2905b6b3fec0ce4d94a6
anandsaikumar <anandsai298@gmail.com>
2023-03-29 16:21:15 +0530

    [sai] Add : validation of registration details like first name last name phone number email id password shold be executed as pattern is given

--- ValidateRegex/ValidateRegex/Program.cs
+++ ValidateRegex/ValidateRegex/Program.cs
@@ -6,14 +6,28 @@ class program
     static void Main(string[] arg)
     {
         ValidatePinCode validatePinCode = new ValidatePinCode();
-        validatePinCode.Validate("400088");
-        validatePinCode.Validate("A533002");
-        validatePinCode.Validate("533002A");
-        validatePinCode.Validate("400 088");
+        validatePinCode.Validate("400088");//pass
+        validatePinCode.Validate("A533002");//fail
+        validatePinCode.Validate("533002A");//fail
+        validatePinCode.Validate("400 088");//pass
         ValidateEmailID validateEmailID = new ValidateEmailID();
-        validateEmailID.ValidateEmail("anandsai298@gmail.com");
-        validateEmailID.ValidateEmail("anandsai298@gmail.co.in");
-        validateEmailID.ValidateEmail("anandsai298@gmai34l.com");
+        validateEmailID.ValidateEmail("anandsai298@gmail.com");//pass
+        validateEmailID.ValidateEmail("anandsai298@gmail.co.in");//pass
+        validateEmailID.ValidateEmail("anandsai298@gmai34l.com");//fail
+        ValidateRegistration validateRegistration = new ValidateRegistration();
+        validateRegistration.Validate_FN("Abc");//pass
+        validateRegistration.Validate_FN("ab");//fail
+        validateRegistration.Validate_FN("Abac");//pass
+        validateRegistration.Validate_LN("Ask");//pass
+        validateRegistration.Validate_LN("ask");//fail
+        validateRegistration.Validate_LN("Anand");//pass
+        validateRegistration.Validate_PhNo("9550631687");//pass
+        validateRegistration.Validate_PhNo("4789251578");//fail
+        validateRegistration.Validate_EmailId("anand298@gmail.co.in");//pass
+        validateRegistration.Validate_EmailId("Ask143@gmail7.com");//fail
+        validateRegistration.Validate_Password("Here1234");//pass
+        validateRegistration.Validate_Password("here#123");//fail
+        validateRegistration.Validate_Password("ASKAR143");//pass
     }
 }
 
--- ValidateRegex/ValidateRegex/ValidateRegistration.cs
+++ ValidateRegex/ValidateRegex/ValidateRegistration.cs
@@ -9,6 +9,54 @@ namespace ValidateRegex
 {
     public class ValidateRegistration
     {
-       
+        public static string NAME = "^[A-Z]{1}[a-z]{2,}$";
+        public static string PHONE_NUMBER = "^[6-9]{1}[0-9]{9}$";
+        public static string EMAIL_ID = "^[0-9A-Za-z]+[.+_]{0,1}[0-9a-zA-Z]+[@][a-zA-Z]+[.][A-Za-z]{2,3}([.][a-zA-Z]{2,3}){0,1}$";
+        public static string PASSWORD = "^[A-z]{1}[a-zA-Z0-9]{7,}$";
+        public void Validate_FN(string fn)
+        {
+            if (Regex.IsMatch(fn, NAME))
+            {
+                Console.WriteLine("Validation of first name is success");
+            }
+            else
+                Console.WriteLine("validation of First name is Unsuccessfull");
+        }
+        public void Validate_LN(string ln)
+        {
+            if (Regex.IsMatch(ln, NAME))
+            {
+                Console.WriteLine("Validation of last name is success");
+            }
+            else
+                Console.WriteLine("validation of last name is Unsuccessfull");
+        }
+        public void Validate_PhNo(string phno)
+        {
+            if (Regex.IsMatch(phno, PHONE_NUMBER))
+            {
+                Console.WriteLine("Validation of phone number is success");
+            }
+            else
+                Console.WriteLine("validation of phone number is Unsuccessfull");
+        }
+        public void Validate_EmailId(string email)
+        {
+            if (Regex.IsMatch(email, EMAIL_ID))
+            {
+                Console.WriteLine("Validation of EMAIL_ID is success");
+            }
+            else
+                Console.WriteLine("validation of EMAIL_ID is Unsuccessfull");
+        }
+        public void Validate_Password(string password)
+        {
+            if (Regex.IsMatch(password, PASSWORD))
+            {
+                Console.WriteLine("Validation of password is success");
+            }
+            else
+                Console.WriteLine("validation of password is Unsuccessfull");
+        }
     }
 }
